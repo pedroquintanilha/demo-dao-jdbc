d03972ea1c1bf04e79bf5e59437903f0d10aa332
Pedro Quintanilha <pedrosquinta@gmail.com>
2022-02-21 11:43:10 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(4, "Books");
+		System.out.println(obj);
+
+	}
+
+}
